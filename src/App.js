04196eb1d16b706fd76c1ffe90ce82d3a1cf6b32
chandratand <Chandra.tand@gmail.com>
2021-11-13 14:40:45 +0700

    restructure project dan menyiapkan utils font dan color

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Splash} from './pages';
+
+const App = () => {
+  return (
+    <View>
+      <Splash />
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
