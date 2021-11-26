dab36d1b19e610822cd6bc23e74c4642b05c1452
chandratand <Chandra.tand@gmail.com>
2021-11-26 17:02:00 +0700

    Login session. jika kita masih login maka setelah splash screen akan masuk ke MainApp, jika belum login maka akan masuk ke halaman GetStarted

--- src/pages/Splash/index.js
+++ src/pages/Splash/index.js
@@ -2,13 +2,25 @@ import React, {useEffect} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {ILLogo} from '../../assets';
 import {colors} from '.././../utils';
+import {Fire} from '../../config';
 
 const Splash = ({navigation}) => {
   useEffect(() => {
-    setTimeout(() => {
-      navigation.replace('GetStarted');
-    }, 3000);
-  });
+    const unsubscribe = Fire.auth().onAuthStateChanged(user => {
+      setTimeout(() => {
+        if (user) {
+          //user lagi login
+          console.log('user: ', user);
+          navigation.replace('MainApp');
+        } else {
+          //user Logout
+          navigation.replace('GetStarted');
+        }
+      }, 3000);
+    });
+
+    return () => unsubscribe();
+  }, [navigation]);
   return (
     <View style={styles.page}>
       <ILLogo />
