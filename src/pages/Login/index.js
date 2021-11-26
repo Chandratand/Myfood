d3a4729a4064933043f55ace574c315b2c2128cb
chandratand <Chandra.tand@gmail.com>
2021-11-26 14:44:26 +0700

    implementasi login dengan firebase

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,21 +2,61 @@ import React from 'react';
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import {ILLogo} from '../../assets';
 import {Button, Gap, Input, Link} from '../../components';
-import {colors, fonts} from '../../utils';
-
+import {colors, fonts, showError, storeData, useForm} from '../../utils';
+import {Fire} from '../../config';
+import {useDispatch} from 'react-redux';
 const Login = ({navigation}) => {
+  const [form, setForm] = useForm({email: '', password: ''});
+  const dispatch = useDispatch();
+
+  const login = () => {
+    console.log('form : ', form);
+    dispatch({type: 'SET_LOADING', value: true});
+
+    Fire.auth()
+      .signInWithEmailAndPassword(form.email, form.password)
+      .then(res => {
+        console.log('success : ', res);
+        dispatch({type: 'SET_LOADING', value: false});
+        setForm('reset');
+        Fire.database()
+          .ref(`users/${res.user.uid}/`)
+          .once('value')
+          .then(resDB => {
+            console.log('data user : ', resDB.val());
+            if (resDB.val()) {
+              storeData('user', resDB.val());
+              navigation.replace('MainApp');
+            }
+          });
+      })
+      .catch(error => {
+        console.log('error : ', error);
+        dispatch({type: 'SET_LOADING', value: false});
+        showError(error.message);
+      });
+  };
   return (
     <View style={styles.page}>
       <ScrollView showsVerticalScrollIndicator={false}>
         <ILLogo />
         <Text style={styles.title}>Masuk dan temukan makananmu</Text>
-        <Input label="Email Address" />
+        <Input
+          label="Email Address"
+          value={form.email}
+          onChangeText={value => setForm('email', value)}
+        />
         <Gap height={40} />
-        <Input label="Password" />
+        <Input
+          label="Password"
+          secureTextEntry
+          value={form.password}
+          onChangeText={value => setForm('password', value)}
+        />
         <Gap height={10} />
         <Link title="Forgot My Password" size={12} />
         <Gap height={40} />
-        <Button title="Sign In" onPress={() => navigation.replace('MainApp')} />
+        <Button title="Sign In" onPress={login} />
         <Gap height={30} />
         <Link
           title="Create New Account"
